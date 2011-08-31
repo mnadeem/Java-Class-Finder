9abafcfbc67addd904bbb4990b30859d5447918b
Nadeem Mohammad <nadeembtech@gmail.com>
2011-08-31 14:48:30 -0700

    Refactored..

--- src/main/java/com/nadeem/app/finder/Finder.java
+++ src/main/java/com/nadeem/app/finder/Finder.java
@@ -8,24 +8,38 @@ import com.nadeem.app.finder.util.DefaultConsoleLogger;
 public class Finder {
 
 	public static void main(String[] args) {
-		
+
 		if (shouldGoCommandLine(args)) {
 			System.out.println("\n**** You are using the Command Line version of Finder !!! ****\n");
 			goCommandLine(args);
 		} else {
-			System.out.println("\n****You are using the GUI version of Finder, Not yet Implemented !!! ****\n");
+			System.out.println("\n**** You are using the GUI version of Finder !!! ****\n");
 			goGUI();
 		}
-	}	
+	}
+
+	private static boolean shouldGoCommandLine(String[] args) {
+		return args.length > 0;
+	}
 
 	private static void goCommandLine(String[] params) {
 		SearchEngine engine =  new SearchEngine(new DefaultConsoleLogger());
 		if (isHelpRequired(params)) {
 			helpUser(params);
-		} else {		
+		} else {
 			engine.searchForClass(params[0], params[1]);
 		}
 		goFurther(engine);
+	}
+
+	private static Boolean isHelpRequired(String[] params) {
+		return params.length != 2;
+	}
+
+	private static void helpUser(String[] params) {
+		System.out.println("It seems you are using the CL Version incorrectly");
+		System.out.println("Correct Format : java -jar finder.jar [location] [ClassName]");
+		System.out.println("Never mind I can simplify It, just follow the instructions ");
 	}
 
 	private static void goFurther(final SearchEngine engine) {
@@ -33,11 +47,11 @@ public class Finder {
 		String location;
 		String className;
 		String userDecision;
-		
-		while(true) {
+
+		while (true) {
 			userDecision = getString("\nDo you want to Contiune ? Enter N for No, Y for Yes", scanner);
 			if ("N".equalsIgnoreCase(userDecision)) {
-				System.out.println("\nBye, bye");
+				System.out.println("\nBye, bye...");
 				break;
 			}
 			location  = getString("Enter Search Location", scanner);
@@ -50,22 +64,9 @@ public class Finder {
 		System.out.print(message + " : ");
 		return scanner.nextLine();
 	}
-	
-	private static Boolean isHelpRequired(String[] params) {
-		return params.length != 2;
-	}
-	
-	private static void helpUser(String[] params) {
-		System.out.println("It seems you are using the CL Version incorrectly");
-		System.out.println("Correct Format : java -jar finder.jar [location] [ClassName]");
-		System.out.println("Never mind I can simplify It, just follow the instructions ");
-	}
 
-	private static boolean shouldGoCommandLine(String[] args) {
-		return args.length > 0;
-	}
-	
 	private static void goGUI() {
-		// TODO Auto-generated method stub		
+		System.out.println("**** Not yet Implemented ****");
+		System.out.println("Bye, bye...");
 	}
 }
